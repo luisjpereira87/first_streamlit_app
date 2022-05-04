14715ed82d098fffc99e4b63feb274c41ad2bdf4
luisjpereira87 <luisjpereira87@gmail.com>
2022-05-04 10:09:04 +0100

    Update streamlit_app.py
    added menu lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents healty Dinner')
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3')
+streamlit.text('Kaile')
+streamlit.text('Hard boiled')
